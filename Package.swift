f2de232bebd458b632727a014bd2e772e4700bff
Jakub Tomanik <j.tomanik@gmail.com>
2016-06-30 14:17:47 +0200

    bump

--- Package.swift
+++ Package.swift
@@ -18,9 +18,8 @@ import PackageDescription
 
 let package = Package(
     name: "KituraMustache",
-    dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git",
-                            majorVersion: 0, minor: 13)])
-
-#if os(OSX)
-    package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/GRMustache.swift.git", majorVersion: 1, minor: 2))
-#endif
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 16),
+        .Package(url: "https://github.com/jtomanik/GRMustache.swift.git", majorVersion: 1, minor: 2)
+        ]
+)
